6886f60b049f94a31f67a7dc54b8fdee7399525e
1i3R <55849349+ArchaicLier@users.noreply.github.com>
2023-11-08 17:36:31 +0000

    init

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include "STask/src/STask/STUtils.h"
+#include <iostream>
+
+using namespace STask;
+
+STask::Task x("Test","Test","11","22") ;
+
+int main(){
+    std::cout<<"Hello PySTask "<<x.name<<'\n';
+}
+
