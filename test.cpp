027fabfc7790b7f1ab992d6159251a88c394b0ed
1i3R <55849349+ArchaicLier@users.noreply.github.com>
2023-11-08 17:50:51 +0000

    update dummy file

--- test.cpp
+++ test.cpp
@@ -1,11 +1,19 @@
 #include "STask/src/STask/STUtils.h"
+#include "STask/src/STask/UserManager.h"
 #include <iostream>
 
 using namespace STask;
 
-STask::Task x("Test","Test","11","22") ;
+STask::Task x("test","test","11","22") ;
+STask::UserManager* um;
 
 int main(){
-    std::cout<<"Hello PySTask "<<x.name<<'\n';
+    um = STask::UserManager::inst();
+
+    const auto& [user, err] 
+        = um->login("Python","123","Fedora");
+
+
+    std::cout<<"Hello PySTask "<<x.name<<'\n'<<err.text<<'\n';
 }
 
